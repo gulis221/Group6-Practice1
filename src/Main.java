3ff80e7b48a4a1e30178534a1f165a6d08fbc15b
Abiel Menghistu <abielmenghistu@gmail.com>
2020-11-12 13:44:50 -0500

    I am getting it,

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("This is group repository");
-        // This is group 6 update.
+        System.out.println("This is team num 6");
+        //This is nice tool, it has taken three days to get it.
+        // Hi team i am getting better;-).
     }
 }
